cda8c937afbe4f9d78a1456bf769e88b721446f0
Sumaya003 <sumayaprakash24@gmail.com>
2026-08-10 19:21:59 +0530

    feat: add service layer with dependency injection

--- src/main/java/com/taskflow/taskflow_pro/controller/TaskController.java
+++ src/main/java/com/taskflow/taskflow_pro/controller/TaskController.java
@@ -1,13 +1,21 @@
 package com.taskflow.taskflow_pro.controller;
 
 
+import com.taskflow.taskflow_pro.service.TaskService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TaskController {
+
+    private final TaskService taskService;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @GetMapping("/api/hello")
     public String hello(){
-        return "Hello from Taskflow Pro";
+
+        return taskService.getHelloMessage();
     }
 }
---
+++ src/main/java/com/taskflow/taskflow_pro/service/TaskService.java
@@ -0,0 +1,10 @@
+package com.taskflow.taskflow_pro.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    public String getHelloMessage(){
+        return "Hello from TaskFlow Pro!";
+    }
+}
