9cef9d650ebc377f6edfe85fd5fc6e87fc11b20c
Sumaya003 <sumayaprakash24@gmail.com>
2026-08-14 01:57:10 +0530

    feat: connect task API to MySQL

--- src/main/java/com/taskflow/taskflow_pro/controller/TaskController.java
+++ src/main/java/com/taskflow/taskflow_pro/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.taskflow.taskflow_pro.controller;
 
 
 import com.taskflow.taskflow_pro.dto.TaskRequest;
+import com.taskflow.taskflow_pro.model.Task;
 import com.taskflow.taskflow_pro.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -22,7 +23,13 @@ public class TaskController {
     }
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/api/tasks")
-    public TaskRequest createTask(@RequestBody @Valid TaskRequest taskRequest){
-        return taskRequest;
+    public Task createTask(@RequestBody @Valid TaskRequest taskRequest){
+
+        Task task = new Task();
+
+        task.setTitle(taskRequest.getTitle());
+        task.setDescription(taskRequest.getDescription());
+        task.setPriority(taskRequest.getPriority());
+        return taskService.saveTask(task);
     }
 }
--- src/main/java/com/taskflow/taskflow_pro/service/TaskService.java
+++ src/main/java/com/taskflow/taskflow_pro/service/TaskService.java
@@ -1,10 +1,23 @@
 package com.taskflow.taskflow_pro.service;
 
+import com.taskflow.taskflow_pro.model.Task;
+import com.taskflow.taskflow_pro.repository.TaskRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TaskService {
-    public String getHelloMessage(){
-        return "Hello from TaskFlow Pro!";
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public String getHelloMessage() {
+        return "Hello from TaskFlow Pro";
+    }
+
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
     }
 }
